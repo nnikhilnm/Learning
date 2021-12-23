c419782cdfccc97a2211f43c79a9c3ae6d19ff9f
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2021-12-23 15:09:29 +0530

    Login

--- student/admin.py
+++ student/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponse
+from .models import *
 
-# Register your models here.
-
+admin.site.register(Profile)
--- student/views.py
+++ student/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
+from django.http import HttpResponse, JsonResponse, Http404
+from .models import *
 
 
 # Create your views here.
@@ -8,9 +10,28 @@ def login(request):
     if request.method=="POST":
         email= request.POST.get('email')
         username= request.POST.get('username')
+        User1= User.objects.all()
+        msg=""
+        val=False
+        for u in User1:
+            print(str(u.username))
+            if str(u.username)==username:
+                val=True
+                break
         password=request.POST.get('password')
-        user=User.objects.create(username=username,email=email,password=password)
-        user.save()
-        return render(request, "login.html")
+        if val:
+            return render(request, "login.html",{'msg':"Username Already Exist!!"})
+        
+        
+        else:
+            user=User.objects.create(username=username,email=email,password=password)
+            user.save()
+            b= authenticate(user)
+            if b is not None:
+                login(request,user)
+                return HttpResponse("PAge not Found")
+                
+            else:
+                return render(request, "profile.html",{'user':request.user})
     else:
         return render(request, "login.html")
