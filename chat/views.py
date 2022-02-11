7352e7f46589e8025a46bc16174ed7795c969cf7
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2022-02-12 00:28:34 +0530

    Abcd

---
+++ chat/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate ,login ,logout
+from django.http import HttpResponse, JsonResponse, Http404
+from .models import *
+from django.core.mail import EmailMessage
+from django.contrib import messages
+from django.conf import settings
+from chat.models import Room, Message
+def home(request):
+    return render(request,"chat/home.html")
+
+def room(request, room):
+    username = request.GET.get('username')
+    room_details = Room.objects.get(name=room)
+    return render(request, 'chat/room.html', {
+        'username': username,
+        'room': room,
+        'room_details': room_details
+    })
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+
+    if Room.objects.filter(name=room).exists():
+        return redirect('/chat/'+room+'/?username='+username)
+    else:
+        new_name = Room.objects.create(name=room)
+        new_name.save()
+        return redirect('/chat/'+room+'/?username='+username)
+
+
+def send(request):
+    message = request.POST['message']
+    username = request.POST['username']
+    room_id = request.POST['room_id']
+
+    new_message = Message.objects.create(value=message, user=username, room=room_id)
+    new_message.save()
+    return HttpResponse('Message sent successfully')
+
+def getMessages(request, room):
+    room_details = Room.objects.get(name=room)
+
+    messages = Message.objects.filter(room=room_details.id)
+    return JsonResponse({"messages":list(messages.values())})
