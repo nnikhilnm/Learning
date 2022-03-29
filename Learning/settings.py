bdec61031d53ff68cc71fa9fe53d1f8899df608d
DarK-VEnOM-16 <75152702+DarK-VEnOM-16@users.noreply.github.com>
2022-03-29 13:55:41 +0530

    sdasd

--- Learning/settings.py
+++ Learning/settings.py
@@ -31,7 +31,7 @@ razorpay_account_id="T7LJuVNrwHkXBnhWkb9qRbQT"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
